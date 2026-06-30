154f8b9c3632db9b84ca26a22b5277d3e1998408
Rasit <rasit@gmail.com>
2026-06-30 10:42:07 +0530

    Fix frontend backend connection

--- smart-freelance-marketplace/frontend/src/api/axios.js
+++ smart-freelance-marketplace/frontend/src/api/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 const token = localStorage.getItem("token");
+
 if (token) {
   api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 }
